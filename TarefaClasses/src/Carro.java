d44ce8e595bb992a584e6c807414471e36083587
LzCosta <116127270+LzCosta@users.noreply.github.com>
2024-09-19 23:39:08 -0300

    Commit inicial

---
+++ TarefaClasses/src/Carro.java
@@ -0,0 +1,23 @@
+public class Carro {
+
+// Classe Carro
+
+    //atributos
+    String nome;
+    String marca;
+    int ano;
+    int vel;
+
+    //metodos
+    void acelerar(int aceleracao) {
+        vel+=aceleracao;
+    }
+
+    void freiar(int reduzir) {
+        vel-=reduzir;
+    }
+
+    void buzinar(){
+        System.out.println("bibibi");
+    }
+}
